164eddaf1678d9dd5c6080921447498ebed6a03d
Roy Le <roy.le@tpfgroup.cc>
2023-07-04 16:41:08 +0700

    first commit

---
+++ js/user.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  loadCompanyTable();
+});
+
+function loadCompanyTable() {
+  $("#user-table").DataTable({
+    responsive: true,
+    order: [
+      [0, "asc"],
+      [1, "asc"],
+      [2, "asc"],
+      [3, "asc"],
+    ],
+    columnDefs: [
+      {
+        targets: [4],
+        searchable: false,
+        sortable: false,
+      },
+    ],
+  });
+}
